75048b4ad1fd1b7aa6e56c44ae1b1e97c583fd00
STINGO <32518650+Stinnngo@users.noreply.github.com>
2026-03-14 18:12:55 +0800

    Update leibniz-simd.swift

--- src/leibniz-simd.swift
+++ src/leibniz-simd.swift
@@ -10,13 +10,15 @@ rounds -= roundsRemainder
 
 rounds += 2 // do this outside the loop
 
+var piVec = SIMD4<Double>(0.0, 0.0, 0.0, 0.0)
 var i: UInt = 2
 while i < rounds {
     let xVec = SIMD4<Double>(-1.0, 1.0, -1.0, 1.0)
     let iVec = SIMD4<Double>(Double(i) + 0, Double(i) + 1, Double(i) + 2, Double(i) + 3)
-    pi += (xVec / (2.0 * iVec - 1.0)).sum()
+    piVec += xVec / (2.0 * iVec - 1.0)
     i += 4
 }
+pi += piVec.sum()
 
 for _ in 0..<roundsRemainder {
     let x = -1.0 + 2.0 * Double(i & 1)
