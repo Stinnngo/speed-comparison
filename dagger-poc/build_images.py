9c6e485deaf1886b65adc3235293d57f5aed8d8c
STINGO <32518650+Stinnngo@users.noreply.github.com>
2026-03-17 06:01:39 +0800

    Update build_images.py

--- dagger-poc/build_images.py
+++ dagger-poc/build_images.py
@@ -112,8 +112,7 @@ async def build_devbox_image(
                 container = container.with_env_variable("CXX", "/nix/store/1ciadcliylyyi9vx21id9vi1p1mayjn8-gcc-wrapper-14.2.0/bin/g++")
                 container = container.with_env_variable("CPP", "/nix/store/1ciadcliylyyi9vx21id9vi1p1mayjn8-gcc-wrapper-14.2.0/bin/cpp")
                 container = container.with_exec(["sh", "-c", "devbox run -- echo $CC && echo $CXX && echo $CPP"])
-                container = container.with_exec(["sh", "-c", "devbox run -- echo /nix/store/1kxmbqsah0bszi95w1ii633vzciqnanx-nix-2.34.0/bin && ls -l /nix/store/1kxmbqsah0bszi95w1ii633vzciqnanx-nix-2.34.0/bin"])
-                container = container.with_exec(["sh", "-c", "devbox run -- echo gcc-root && ls $(which gcc)/.."])
+                container = container.with_exec(["sh", "-c", "devbox run -- cd $(dirname $(which gcc)) && pwd && ls && ls bin"])
                 # container = container.with_exec(["sh", "-c", "devbox run -- clang --version"])
             container = container.with_exec(
                 ["sh", "-c", f"devbox add {packages_str} {insecure_flags}"]
