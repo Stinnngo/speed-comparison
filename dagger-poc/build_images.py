69c20813b2e9e80e05490ea0cd275ef6dea7e0c9
STINGO <32518650+Stinnngo@users.noreply.github.com>
2026-03-17 07:34:20 +0800

    Update build_images.py

--- dagger-poc/build_images.py
+++ dagger-poc/build_images.py
@@ -111,9 +111,11 @@ async def build_devbox_image(
                 container = container.with_env_variable("CC", "/nix/store/1ciadcliylyyi9vx21id9vi1p1mayjn8-gcc-wrapper-14.2.0/bin/gcc")
                 container = container.with_env_variable("CXX", "/nix/store/1ciadcliylyyi9vx21id9vi1p1mayjn8-gcc-wrapper-14.2.0/bin/g++")
                 container = container.with_env_variable("CPP", "/nix/store/1ciadcliylyyi9vx21id9vi1p1mayjn8-gcc-wrapper-14.2.0/bin/cpp")
+                container = container.with_env_variable("GCC_14_PATH", "/nix/store/1ciadcliylyyi9vx21id9vi1p1mayjn8-gcc-wrapper-14.2.0")
+                container = container.with_env_variable("GCC_15_PATH", "/nix/store/kbw2j1vag664b3sj3rjwz9v53cqx87sb-gcc-wrapper-15.2.0")
                 container = container.with_exec(["sh", "-c", "devbox run -- echo $CC && echo $CXX && echo $CPP"])
-                container = container.with_exec(["sh", "-c", "devbox run -- cd && cd .. && cd nix/store/kbw2j1vag664b3sj3rjwz9v53cqx87sb-gcc-wrapper-15.2.0/bin && ls && ls .."])
-                container = container.with_exec(["sh", "-c", "devbox run -- cd && cd .. && cd nix && pwd && ls && ls store"])
+                container = container.with_exec(["sh", "-c", "devbox run -- mkdir -p $GCC_14_PATH/bin && ls $GCC_14_PATH"])
+                container = container.with_exec(["sh", "-c", "devbox run -- rm -rf $GCC_15_PATH/bin && ln -s $GCC_14_PATH/bin $GCC_15_PATH/bin && ls $GCC_15_PATH"])
                 # container = container.with_exec(["sh", "-c", "devbox run -- clang --version"])
             container = container.with_exec(
                 ["sh", "-c", f"devbox add {packages_str} {insecure_flags}"]
